dd680514e94e0d92ff4e40b47762fd7a49aa2abf
Samantha Laine <samanthalaine8@gmail.com>
2021-07-23 09:23:39 -0400

    commit tree 04a15a22ebcd89442c0c2db23418de82534156b7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let match = drivers.filter(driverName => driverName === name);
+    return match;
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.substring(0, name.length) === name)
+}
